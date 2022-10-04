e1afbc2a0b2b0a4ed252ae688814e54125a8fa1e
Lourdes <lpozfer614@g.educaand.es>
2022-10-04 20:15:02 +0200

    Versión con array de errores.

--- formulario.php
+++ formulario.php
@@ -11,13 +11,13 @@
         $errores=[];
         if(isset($_POST['enviar'])){
             if (empty($_POST['nombre'])){
-                $errores['nombre']="Nombre vacío";
+                $errores['nombre']='<span style="color:red">Nombre vacío</span><br>';
             }
             if(empty($_POST['apellidos'])){
-                $errores['apellidos']="Apellidos vacío";
+                $errores['apellidos']='<span style="color:red">Debe introducir los apellidos</span><br>';
             }
-            if(isset($_POST['enviar']) && empty($_POST['edad'])){
-                $errores['edad']="Edad vacío";
+            if(empty($_POST['edad'])){
+                $errores['edad']='<span style="color:red">Debe introducir una edad</span><br>';
             }
         }
     ?>
@@ -25,23 +25,23 @@
         <label >Nombre:</label><br>
         <input type="text" name="nombre" value="<?php if(isset($_POST['nombre']))echo $_POST['nombre'];?>"/><br>
         <?php 
-            if(isset($_POST['enviar']) && empty($_POST['nombre'])){
+            if(empty($_POST['nombre'])){
                 echo $errores['nombre'] ;
             }
         ?>
         <label >Apellidos:</label><br>
         <input type="text" name="apellidos" value="<?php if(isset($_POST['apellidos']))echo $_POST['apellidos'];?>"/><br>
         <?php 
-            if(isset($_POST['enviar']) && empty($_POST['apellidos'])){
-                echo '<span style="color:red">Debe introducir los apellidos</span><br>';
+            if(empty($_POST['apellidos'])){
+                echo $errores['apellidos'] ;
             }
         ?>
 
         <label >Edad:</label><br>
         <input type="text" name="edad" value="<?php if(isset($_POST['edad']))echo $_POST['edad'];?>"/><br>
         <?php 
-            if(isset($_POST['enviar']) && empty($_POST['edad'])){
-                echo '<span style="color:red">Debe introducir una edad</span><br>';
+            if(empty($_POST['edad'])){
+                echo $errores['edad'] ;
             }
         ?>
 
